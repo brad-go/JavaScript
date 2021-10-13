373103477247182be7dc81ed192483f57859c958
고동현 <dhjk35@naver.com>
2021-10-13 23:09:36 +0900

    update pingpong

--- PingPong/app.js
+++ PingPong/app.js
@@ -1,15 +1,25 @@
-const playerOneScore = document.querySelector('#playerOneScore');
-const playerTwoScore = document.querySelector('#playerTwoScore');
-const btnOne = document.querySelector('#playerOneBtn');
-const btnTwo = document.querySelector('#playerTwoBtn');
-const btnReset = document.querySelector('#resetBtn');
+const p1 = {
+  score: 0,
+  display: document.querySelector('#p1Display'),
+  button: document.querySelector('#p1Btn')
+}
+
+const p2 = {
+  score: 0,
+  display: document.querySelector('#p2Display'),
+  button: document.querySelector('#p2Btn')
+}
+
+const resetBtn = document.querySelector('#resetBtn');
 const winningScoreSelect = document.querySelector('#playto');
 
 // 최대 점수를 기본적으로 설정 = 6
 let winningScore = parseInt(winningScoreSelect.value);
 // 각 플레이어의 점수를 담을 변수 설정
-let pointOne = 0;
-let pointTwo = 0;
+let p1Score = 0;
+let p2Score = 0;
+// 게임 진행 여부
+let isGameOver = false;
 
 // winningScoreSelect의 값이 변경되면 점수 최대치를 제한하기 위해 작성
 winningScoreSelect.addEventListener('change', function() {
@@ -18,66 +28,42 @@ winningScoreSelect.addEventListener('change', function() {
   reset();
 })
 
-// 버튼 비활성화 함수
-function btnDisabled() {
-  btnOne.setAttribute('disabled', 'true');
-  btnTwo.setAttribute('disabled', 'true');
-}
 
-// 버튼 활성화 함수
-function btnAbled() {
-  // 요소의 속성을 제거하는 메서드 removeAttribute();
-  btnOne.removeAttribute('disabled');
-  btnTwo.removeAttribute('disabled');
-}
-
-// 게임을 이겼을 때 효과를 주기위한 함수
-function gameWin(winner, loser) {
-  winner.classList.add('winner');
-  loser.classList.add('loser');
-  btnOne.classList.add('end-game');
-  btnTwo.classList.add('end-game');
-  btnDisabled();
-}
-
-btnOne.addEventListener('click', () => {
-  // player 1의 점수를 1 올린다.
-  pointOne++;
-  playerOneScore.innerText = pointOne;
-  // 점수가 최대치에 도달하면 게임 종료!
-  if(pointOne === winningScore) {
-    gameWin(playerOneScore, playerTwoScore);
+function updateScores(player, opponent) {
+  if(!isGameOver) {
+    // player 1의 점수를 1 올린다.
+    player.score++;
+    // 점수가 최대치에 도달하면 게임 종료!
+    if(player.score === winningScore) {
+      isGameOver = true;
+      player.display.classList.add('winner');
+      opponent.display.classList.add('loser');
+      player.button.classList.add('end-game');
+      opponent.button.classList.add('end-game');
+      player.button.disabled = true;
+      opponent.button.disabled = true;
+    }
+    player.display.textContent = player.score;
   }
+}
+
+p1.button.addEventListener('click', () => {
+  updateScores(p1, p2);
 })
 
-btnTwo.addEventListener('click', () => {
-  // plaeyr 2의 점수를 1 올린다.
-  pointTwo++;
-  playerTwoScore.innerText = pointTwo;
-  // 점수가 최대치에 도달하면 게임 종료!
-  if(pointTwo === winningScore) {
-    gameWin(playerTwoScore, playerOneScore);
-  }
+p2.button.addEventListener('click', () => {
+  updateScores(p2, p1);
 })
 
 function reset() {
-  pointOne = 0; 
-  playerOneScore.innerText = pointOne;
-  pointTwo = 0;
-  playerTwoScore.innerText = pointTwo;
-  btnAbled();
+  isGameOver = false;
+  for(let p of [p1, p2]) {
+    p.score = 0;   
+    p.display.textContent = p.score;
+    p.display.classList.remove('winner', 'loser');
+    p.button.classList.remove('end-game');
+    p.button.disabled = false;
+  }
 }
 
-// 게임 초기화를 위한 함수
-function resetGame(winner, loser) {
-  winner.classList.remove('winner');
-  loser.classList.remove('loser');
-  btnOne.classList.remove('end-game');
-  btnTwo.classList.remove('end-game');
-}
-
-btnReset.addEventListener('click', () => {
-  pointOne >= winningScore ? resetGame(playerOneScore, playerTwoScore) : resetGame(playerTwoScore, playerOneScore);
-  // 추가된 클래스들은 winningScore에 도달해야 변경되기 때문에 중간에 리셋을 눌러도 초기 상태와 같을 수 있다!
-  reset();
-})
+resetBtn.addEventListener('click', reset);
