3b5f270846a8ca7b22f9bcd434fc8b7c872ff026
고동현 <dhjk35@naver.com>
2021-10-13 21:12:35 +0900

    update pingpong

--- PingPong/app.js
+++ PingPong/app.js
@@ -3,18 +3,19 @@ const playerTwoScore = document.querySelector('#playerTwoScore');
 const btnOne = document.querySelector('#playerOneBtn');
 const btnTwo = document.querySelector('#playerTwoBtn');
 const btnReset = document.querySelector('#resetBtn');
-const select = document.querySelector('#points');
+const winningScoreSelect = document.querySelector('#playto');
 
 // 최대 점수를 기본적으로 설정 = 6
-let maxPoint = parseInt(select.value);
+let winningScore = parseInt(winningScoreSelect.value);
 // 각 플레이어의 점수를 담을 변수 설정
 let pointOne = 0;
 let pointTwo = 0;
 
-// select의 값이 변경되면 점수 최대치를 제한하기 위해 작성
-select.addEventListener('change', () => {
-  console.log(select.value);
-  maxPoint = parseInt(select.value);
+// winningScoreSelect의 값이 변경되면 점수 최대치를 제한하기 위해 작성
+winningScoreSelect.addEventListener('change', function() {
+  winningScore = parseInt(this.value);
+  // winningScore를 설정하면 게임을 초기화
+  reset();
 })
 
 // 버튼 비활성화 함수
@@ -39,20 +40,12 @@ function gameWin(winner, loser) {
   btnDisabled();
 }
 
-// 게임 초기화를 위한 함수
-function resetGame(winner, loser) {
-  winner.classList.remove('winner');
-  loser.classList.remove('loser');
-  btnOne.classList.remove('end-game');
-  btnTwo.classList.remove('end-game');
-}
-
 btnOne.addEventListener('click', () => {
   // player 1의 점수를 1 올린다.
   pointOne++;
   playerOneScore.innerText = pointOne;
   // 점수가 최대치에 도달하면 게임 종료!
-  if(pointOne >= maxPoint) {
+  if(pointOne === winningScore) {
     gameWin(playerOneScore, playerTwoScore);
   }
 })
@@ -62,17 +55,29 @@ btnTwo.addEventListener('click', () => {
   pointTwo++;
   playerTwoScore.innerText = pointTwo;
   // 점수가 최대치에 도달하면 게임 종료!
-  if(pointTwo >= maxPoint) {
+  if(pointTwo === winningScore) {
     gameWin(playerTwoScore, playerOneScore);
   }
 })
 
-btnReset.addEventListener('click', () => {
-  pointOne >= maxPoint ? resetGame(playerOneScore, playerTwoScore) : resetGame(playerTwoScore, playerOneScore);
-  // 추가된 클래스들은 maxPoint에 도달해야 변경되기 때문에 중간에 리셋을 눌러도 초기 상태와 같을 수 있다!
+function reset() {
   pointOne = 0; 
   playerOneScore.innerText = pointOne;
   pointTwo = 0;
   playerTwoScore.innerText = pointTwo;
   btnAbled();
+}
+
+// 게임 초기화를 위한 함수
+function resetGame(winner, loser) {
+  winner.classList.remove('winner');
+  loser.classList.remove('loser');
+  btnOne.classList.remove('end-game');
+  btnTwo.classList.remove('end-game');
+}
+
+btnReset.addEventListener('click', () => {
+  pointOne >= winningScore ? resetGame(playerOneScore, playerTwoScore) : resetGame(playerTwoScore, playerOneScore);
+  // 추가된 클래스들은 winningScore에 도달해야 변경되기 때문에 중간에 리셋을 눌러도 초기 상태와 같을 수 있다!
+  reset();
 })
